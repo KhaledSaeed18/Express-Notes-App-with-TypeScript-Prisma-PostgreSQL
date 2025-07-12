e395d32a2deedfd11e724705f87e26eb4cae54bf
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-07-12 17:54:39 +0300

    refactor: add documentation for prismaClient.ts

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -1,3 +1,9 @@
+/*
+    * src/database/prismaClient.ts
+    * This file initializes the Prisma Client and connects to the database.
+    * It exports the Prisma Client instance for use in other parts of the application.
+*/
+
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
